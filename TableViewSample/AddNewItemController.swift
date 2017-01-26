802f26b0f16e3f878ec8cc42b8f5aae36c8d4ac8
Hitoshi Okawa <14all.okawa@gmail.com>
2017-01-26 17:35:56 +0900

    Enable to dismiss

---
+++ TableViewSample/AddNewItemController.swift
@@ -0,0 +1,25 @@
+//
+//  AddNewItemController.swift
+//  TableViewSample
+//
+//  Created by Okawa Hitoshi on 2017/01/26.
+//  Copyright © 2017年 HitoshiOkawa. All rights reserved.
+//
+
+import UIKit
+
+class AddNewItemController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
