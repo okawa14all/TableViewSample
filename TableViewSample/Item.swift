db5f1b1deef151f6ebf1882b211ec42d6eead1eb
Hitoshi Okawa <14all.okawa@gmail.com>
2017-01-26 22:20:57 +0900

    Add Item model class

---
+++ TableViewSample/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  TableViewSample
+//
+//  Created by Okawa Hitoshi on 2017/01/26.
+//  Copyright © 2017年 HitoshiOkawa. All rights reserved.
+//
+
+class Item {
+    var title = ""
+    var checked = false
+
+    init(title: String) {
+        self.title = title
+    }
+}
--- TableViewSample/ViewController.swift
+++ TableViewSample/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBOutlet var tableView: UITableView!
 
-    var labels = ["item 1", "item 2", "item 3"]
+    var items = [Item(title: "item 1"), Item(title: "item 2"), Item(title: "item 3")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,19 +23,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return labels.count
+        return items.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath)
         let label = cell.viewWithTag(1) as! UILabel
-        label.text = labels[indexPath.row]
+        label.text = items[indexPath.row].title
         return cell
     }
 
     func addNewItem(item: String) {
         print("***" + item)
-        labels.append(item)
+        items.append(Item(title: item))
         self.tableView.reloadData()
     }
 
